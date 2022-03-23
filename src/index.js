84944a29ec943c71731d595be6c615a29873569d
joaoMarinho94 <joaopaulim94@gmail.com>
2022-03-23 18:19:04 -0300

    feat: create delete route to todo

--- src/index.js
+++ src/index.js
@@ -93,7 +93,18 @@ app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+
+  if (todoIndex < 0) {
+    return response.status(400).json({ error: "Todo not found" });
+  }
+
+  user.todos.splice(todoIndex, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
